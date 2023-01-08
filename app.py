b5d058544f8d578e5f01b65fcbda745057a4b3ac
Coding Professor <82854291+coding-professor@users.noreply.github.com>
2023-01-08 11:32:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def load_data():
     data = soup.find('script', id='__NEXT_DATA__', type='application/json')
     coins = {}
     coin_data = json.loads(data.contents[0])
-    #listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']
+    listings = coin_data['props']['initialState']
     
-    st.write(coin_data['props']['initialState'])
+    st.write(listings['cryptocurrency'])
     
 load_data()
