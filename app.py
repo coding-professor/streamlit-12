ada72395db8f6dade3e44bbfc66d1155f3642c27
Coding Professor <82854291+coding-professor@users.noreply.github.com>
2023-01-08 11:51:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,7 @@ def load_data():
     coin_data = json.loads(data.contents[0])
     listings = json.loads(coin_data['props']['initialState'])
     listings2 = listings['cryptocurrency']['listingLatest']['data']
-    
-    for i in listings2:
-      coins[str(i['id'])] = i['slug']
+   
     
     st.write(listings2)
     
