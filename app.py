fb3f6868899151dcb3c9538c14acdec3e909e13c
Coding Professor <82854291+coding-professor@users.noreply.github.com>
2023-01-08 11:40:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ def load_data():
     listings = coin_data['props']['initialState']
     
  
-    st.write(json.loads(listings))
+    st.write(json.loads(listings["cryptocurrency"]))
     
 load_data()
